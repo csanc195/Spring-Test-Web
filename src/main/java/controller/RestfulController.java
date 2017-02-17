3cbd886fc6937526a2f245f28f579bfc811f2f6a
Carlos Sanchez <CarlosSanchez@carloss-mbp-3.attlocal.net>
2017-02-17 13:54:35 -0500

    generic

--- src/main/java/controller/RestfulController.java
+++ src/main/java/controller/RestfulController.java
@@ -4,7 +4,6 @@ import dto.Greeting;
 import dto.IP_Reccords;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
